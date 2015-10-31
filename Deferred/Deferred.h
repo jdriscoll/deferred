4880d73199eb21ba82c904272270bfecff6eaefd
Justin Driscoll <justin@driscolldev.com>
2015-10-31 13:54:33 -0400

    Initial Commit

---
+++ Deferred/Deferred.h
@@ -0,0 +1,19 @@
+//
+//  Deferred.h
+//  Deferred
+//
+//  Created by Justin Driscoll on 10/31/15.
+//  Copyright © 2015 Retrobit, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Deferred.
+FOUNDATION_EXPORT double DeferredVersionNumber;
+
+//! Project version string for Deferred.
+FOUNDATION_EXPORT const unsigned char DeferredVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Deferred/PublicHeader.h>
+
+
